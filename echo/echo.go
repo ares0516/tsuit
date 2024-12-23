356eb006a9a02e0bec550150e9867c6606d8a8f1
zhouzm <zhouzm@infogo.com.cn>
2024-12-23 16:30:49 +0800

    reply add server time

--- echo/echo.go
+++ echo/echo.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Write "Hello World" as the response
-	fmt.Fprintf(w, "Hello World")
+	// Write "Hello World" and the current server time as the response
+	fmt.Fprintf(w, "Hello World\n")
+	fmt.Fprintf(w, "Current server time: %s", time.Now().Format(time.RFC1123))
 }
 
 func main() {
